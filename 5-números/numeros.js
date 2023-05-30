38134c7d25989722a65c4e9e251043de183d3a0d
Rafa <rafacv23@gmail.com>
2023-05-30 23:35:09 +0200

    31-05-2023

---
+++ 5-números/numeros.js
@@ -0,0 +1,17 @@
+let alturaCm = 175;
+
+let alturaM = 1.75;
+
+let pesoKg = 75.3;
+
+let alturaRedondeada = Math.round(alturaM)
+console.log(alturaRedondeada)
+
+let pesoRedondeado = Math.trunc(pesoKg)
+console.log(pesoRedondeado)
+
+const maxValor = Number.MAX_VALUE;
+const suma = maxValor + 1;
+const esIgual = suma === maxValor;
+
+console.log(esIgual);
