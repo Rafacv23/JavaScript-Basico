1a87d22ceb98e222c1856cd7abbbd9e8118df054
Rafa <rafacv23@gmail.com>
2023-05-30 18:04:00 +0200

    30-05-2023

---
+++ 4-strings/strings.js
@@ -0,0 +1,29 @@
+let nombre = "Rafa";
+console.log(nombre);
+
+let apellido = "Canosa";
+console.log(apellido);
+
+let estudiante = nombre + " " + apellido;
+console.log(estudiante);
+
+let estudianteMayus = estudiante.toUpperCase(estudiante)
+console.log(estudianteMayus);
+
+let estudianteMinus = estudiante.toLowerCase(estudiante)
+console.log(estudianteMinus);
+
+let numEstudiante = estudiante.length
+console.log(numEstudiante);
+
+let firstNombre = nombre.slice(0,1);
+console.log(firstNombre);
+
+let lastApellido = apellido.slice(5);
+console.log(lastApellido);
+
+let estudianteEspacios = nombre.concat(apellido);
+console.log(estudianteEspacios)
+
+let isNombre = estudiante.includes(nombre);
+console.log(isNombre);
